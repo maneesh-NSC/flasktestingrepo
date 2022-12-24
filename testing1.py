ce466af488430faf421dee764fec438cfce58b02
maneesh-NSC <maneesh@networksecuritycloud.com>
2022-12-24 18:09:10 -0500

    initial local repo

---
+++ testing1.py
@@ -0,0 +1,17 @@
+import time
+from flask import Flask
+import pandas as pd
+import sys
+
+
+print(time.altzone)
+
+application = Flask(__name__)
+@application.route('/')
+def hello_world():
+    return "Subscribe"
+
+
+
+
+
